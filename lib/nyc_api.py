ede14126459c70239948906e68d5836d0c3b23aa
Kirimino <brianonduso08@gmail.com>
2024-07-07 09:41:02 +0300

    complete lab

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -1,14 +1,28 @@
-
 import requests
 import json
 
 class GetPrograms:
 
-  def get_programs(self):
-    URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+    def get_programs(self):
+        URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+        response = requests.get(URL)
+        return response.json()  # Parse JSON directly into Python dictionary
 
-    response = requests.get(URL)
-    return response.content
+    def program_agencies(self):
+        programs_list = []
+        programs = self.get_programs()
+        for program in programs:
+            if "agency" in program:  # Check if 'agency' key exists in the program dictionary
+                programs_list.append(program["agency"])
 
-programs = GetPrograms().get_programs()
-print(programs)
+        return programs_list
+
+# Instantiate GetPrograms class
+programs = GetPrograms()
+
+# Get the list of agencies
+agencies = programs.program_agencies()
+
+# Print each agency (removing duplicates with set)
+for agency in set(agencies):
+    print(agency)
